608a62bd6a10860cd62b3cb7b23eef90069573dc
MineGame159 <petulko08@gmail.com>
2025-08-30 20:14:28 +0200

    Twice

--- common/src/main/java/minegame159/spacemod/SpaceMod.java
+++ common/src/main/java/minegame159/spacemod/SpaceMod.java
@@ -6,6 +6,7 @@ import minegame159.spacemod.worldgen.feature.ModFeatures;
 import minegame159.spacemod.worldgen.placement.ModPlacements;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import org.slf4j.Logger;
 
 public final class SpaceMod {
     public static final String ID = "spacemod";
