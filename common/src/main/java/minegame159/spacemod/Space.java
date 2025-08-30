9e6618482d810acd2acdc36bea79f457611cbdc3
airplane <31675260+airplane@users.noreply.github.com>
2025-08-29 22:59:40 -0400

    Make players take damage in Space

--- common/src/main/java/minegame159/spacemod/Space.java
+++ common/src/main/java/minegame159/spacemod/Space.java
@@ -5,6 +5,8 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
@@ -20,6 +22,13 @@ public final class Space {
         ResourceLocation.fromNamespaceAndPath(SpaceMod.ID, "space")
     );
 
+    public static final ResourceKey<DamageType> SUFFOCATION_DAMAGE = ResourceKey.create(
+        Registries.DAMAGE_TYPE,
+        ResourceLocation.fromNamespaceAndPath(SpaceMod.ID, "space_suffocation")
+    );
+
+    private static DamageSource suffocationDamage;
+
     public static void init() {
         TickEvent.SERVER_LEVEL_POST.register(Space::onLevelTickPost);
     }
@@ -28,7 +37,17 @@ public final class Space {
         if (!level.isClientSide && level.dimension().location().equals(KEY.location())) {
             List<Player> playersToTeleport = null;
 
+            if (suffocationDamage == null) {
+                suffocationDamage = new DamageSource(level.registryAccess()
+                    .registry(Registries.DAMAGE_TYPE)
+                    .orElseThrow().getHolderOrThrow(SUFFOCATION_DAMAGE)
+                );
+            }
+
             for (var player : level.players()) {
+                if (!player.isInvulnerable())
+                    player.hurt(suffocationDamage, 1);
+
                 if (player.getY() < 0) {
                     if (playersToTeleport == null) {
                         playersToTeleport = new ArrayList<>();
